6e917091422b2136c471b00815fc551545221281
Jeremie Bolduc <jeremiebolducjs@gmail.com>
2024-02-03 21:02:50 -0500

    feature: server integration

--- Godot/Application/start_recyling.gd
+++ Godot/Application/start_recyling.gd
@@ -1,6 +1,6 @@
 extends Control
 
-var image = null
+var file = null
 var globalPath = ""
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -24,17 +24,38 @@ func _on_upload_an_image_pressed():
 func _on_file_dialog_file_selected(path):
 	enable_and_show_node($VBoxContainer/Confirm)
 	globalPath = path
-	image = Image.new()
-	image.load(path)
+	file = FileAccess.open(path, FileAccess.READ)
 	
 	var image_texture = ImageTexture.new()
-	image_texture.set_image(image)
+	image_texture.set_image(file)
 	
 	$ColorRect/TextureRect.texture = image_texture
 
 
 func _on_confirm_pressed():
-	if image != null && globalPath != '':
-		var imageData = image.get_data()
-		var imageBase64 = Marshalls.raw_to_base64(imageData)
+	if file != null && globalPath != '':
+		var bytes = FileAccess.get_file_as_bytes(globalPath)
+		var content = Marshalls.raw_to_base64(bytes)
+		var payload = { "image": content }
+
+		var http_request = HTTPRequest.new()
+		add_child(http_request)
+		http_request.request_completed.connect(self._on_request_completed)
 		
+		var status = http_request.request(
+			"http://localhost:8000/image",
+			[], 
+			HTTPClient.METHOD_POST, 
+			JSON.stringify(payload)
+		)
+		
+		if status != OK:
+			push_error("An error occurred in the HTTP request.")
+		
+
+func _on_request_completed(result, response_code, headers, body):
+	if response_code == 200:
+		print("Request successful")
+		print("Response body: ", body)
+	else:
+		print("Request failed. Response code: ", response_code)
