63f37fd717ad0c43b0ab4ddfadb3c7fb2b102822
iremyildizz <iremyildiz02@gmail.com>
2024-02-04 10:43:41 -0500

    How to recycle added

---
+++ Godot/Application/how_to_recycle.gd
@@ -0,0 +1,22 @@
+extends Control
+
+@export_file("*.tscn") var urlToPlastic: String
+@export_file("*.tscn") var urlToMetal: String
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+
+func _on_plastic_2_pressed():
+	get_tree().change_scene_to_file(urlToPlastic)
+
+
+func _on_metal_2_pressed():
+	get_tree().change_scene_to_file(urlToMetal)
