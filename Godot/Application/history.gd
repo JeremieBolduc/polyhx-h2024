e9b7b99ef097c76a430a94110239942117a21540
Pablo <pablo.cabale@polymtl.ca>
2024-02-04 09:49:06 -0500

    config

--- Godot/Application/history.gd
+++ Godot/Application/history.gd
@@ -1,6 +1,6 @@
 extends Control
 
-
+var itemList
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # bd
@@ -9,3 +9,24 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+func getItemList():
+	pass
+	#var bytes = FileAccess.get_file_as_bytes(globalPath)
+	#var content = Marshalls.raw_to_base64(bytes)
+	#var payload = { "image": content }
+#
+	#var http_request = HTTPRequest.new()
+	#add_child(http_request)
+	#http_request.request_completed.connect(self._on_request_completed)
+	#
+	#var headers = ["Content-Type: application/json;"]
+	#var status = http_request.request(
+		#"http://localhost:8000/items",
+		#headers,
+		#HTTPClient.METHOD_POST,
+		#JSON.stringify(payload)
+	#)
+	#
+	#if status != OK:
+		#push_error("An error occurred in the HTTP request.")
