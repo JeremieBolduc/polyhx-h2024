b4dfcc341c50a3226a445d21d775bbbbb8afe1a5
iremyildizz <iremyildiz02@gmail.com>
2024-02-04 11:12:35 -0500

    TaskBar added

--- Godot/Application/template.gd
+++ Godot/Application/template.gd
@@ -2,7 +2,10 @@ extends Control
 
 @export_file("*.tscn") var urlToMainMenu: String
 @export_file("*.tscn") var mainMenu: String
-var richTextLabel
+@export_file("*.tscn") var about: String
+@export_file("*.tscn") var howTo: String
+@export_file("*.tscn") var history: String
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -19,3 +22,20 @@ func _on_go_back_pressed():
 
 func _on_texture_button_pressed():
 	get_tree().change_scene_to_file(mainMenu)
+
+
+
+func _on_aboutus_pressed():
+	get_tree().change_scene_to_file(about)
+
+
+func _on_home_pressed():
+	get_tree().change_scene_to_file(mainMenu)
+
+
+func _on_howto_pressed():
+	get_tree().change_scene_to_file(howTo)
+
+
+func _on_history_pressed():
+	get_tree().change_scene_to_file(history)
