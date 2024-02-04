4c6cfe3b2c985b1983e64f24b6fa6c747197d8b9
Pablo <pablo.cabale@polymtl.ca>
2024-02-04 09:58:42 -0500

    call bd

--- Godot/Application/history.gd
+++ Godot/Application/history.gd
@@ -1,32 +1,30 @@
 extends Control
 
 var itemList
-# Called when the node enters the scene tree for the first time.
+
 func _ready():
-	pass # bd
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+	getItemList()
 
 func getItemList():
-	pass
-	#var bytes = FileAccess.get_file_as_bytes(globalPath)
-	#var content = Marshalls.raw_to_base64(bytes)
-	#var payload = { "image": content }
-#
-	#var http_request = HTTPRequest.new()
-	#add_child(http_request)
-	#http_request.request_completed.connect(self._on_request_completed)
-	#
-	#var headers = ["Content-Type: application/json;"]
-	#var status = http_request.request(
-		#"http://localhost:8000/items",
-		#headers,
-		#HTTPClient.METHOD_POST,
-		#JSON.stringify(payload)
-	#)
-	#
-	#if status != OK:
-		#push_error("An error occurred in the HTTP request.")
+	print("start")
+	var http_request = HTTPRequest.new()
+	add_child(http_request)
+	http_request.request_completed.connect(self._on_request_completed)
+	
+	var headers = ["Content-Type: application/json;"]
+	var status = http_request.request(
+		"http://localhost:8000/items",
+		headers,
+		HTTPClient.METHOD_GET
+	)
+	print("requested")
+	
+	if status != OK:
+		push_error("An error occurred in the HTTP request.")
+		
+func _on_request_completed(result, response_code, headers, body):
+	print("completed")
+	if response_code == 200:
+		print(body.get_string_from_utf8())
+	else:
+		print("Request failed. Response code: ", response_code)
